39faa9f1a29763780ef2511ae7a8d4c2a749ca29
Mike Oliveira <mikeoliveira51@gmail.com>
2021-03-13 00:43:14 -0300

    Commit 18

--- Statement.java
+++ Statement.java
@@ -1,3 +1,20 @@
-public class Statement {
+import java.util.Enumeration;
 
+public abstract class Statement {
+	public abstract String getHeader(Customer aCustomer);
+    public abstract String getRentalFigures(Rental aRental);
+    public abstract String getFooter(Customer aCustomer);
+	
+    public String value(Customer aCustomer) {
+        Enumeration rentals = aCustomer.getRentals();
+        String result = getHeader(aCustomer);
+        while (rentals.hasMoreElements()) {
+           Rental each = (Rental) rentals.nextElement();
+           //show figures for each rental
+           result += getRentalFigures(each);
+        }
+        //add footer lines
+        result += getFooter(aCustomer);
+        return result;
+    }
 }
