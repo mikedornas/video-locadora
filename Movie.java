3232f2b969028ebcfe146ce9eb3afc6b2148cc25
Mike Oliveira <mikeoliveira51@gmail.com>
2021-03-12 23:07:35 -0300

    Commit 9

--- Movie.java
+++ Movie.java
@@ -27,15 +27,15 @@ public class Movie {
   public double getCharge(int daysRented) {
      double thisAmount = 0;
      switch (getPriceCode()) {
-        case Movie.REGULAR:
+        case REGULAR:
            thisAmount += 2;
            if (daysRented > 2)
               thisAmount += (daysRented - 2) * 1.5;
            break;
-        case Movie.NEW_RELEASE:
+        case NEW_RELEASE:
            thisAmount += daysRented * 3;
            break;
-        case Movie.CHILDRENS:
+        case CHILDRENS:
            thisAmount += 1.5;
            if (daysRented > 3)
               thisAmount += (daysRented - 3) * 1.5;
